deed3d5459f0edf244ebd9a9759ceaea4380df89
Min Khaung <minkhaung.mkks@gmail.com>
2023-05-08 19:11:24 +0630

    Initialize App

---
+++ app/layout.jsx
@@ -0,0 +1,24 @@
+import React from 'react'
+import '@styles/globals.css'
+
+export const metadata = {
+    title: "Prompt Paradise",
+    description: "Your 1 stop for prompts."
+}
+
+const RootLayout = ({children}) => {
+  return (
+    <html lang="en">
+        <body>
+            <div className="main">
+                <div className="gradient"></div>
+            </div>
+            <main className="app">
+                {children}
+            </main>
+        </body>
+    </html>
+  )
+}
+
+export default RootLayout
