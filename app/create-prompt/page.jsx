b9612fe912b39e3459e34a69a563c4c4f3e8b605
Min Khaung <minkhaung.mkks@gmail.com>
2023-05-11 17:54:55 +0630

    Create Prompt Visual Lastest

--- app/create-prompt/page.jsx
+++ app/create-prompt/page.jsx
@@ -1,8 +1,32 @@
-import React from 'react'
+"use client"
+
+import React, {useState} from 'react'
+import { useSession } from 'next-auth/react'
+import { useRouter } from 'next/router'
+
+import Form from '@components/Form'
 
 const CreatePrompt = () => {
+    const [submitting, setSubmitting] = useState(false)
+    const [post, setPost] = useState({
+        prompt: '',
+        tag:''
+    })
+
+    const createPrompt= async (e)=>{
+
+    }
+
     return (
-        <div>Create Prompt</div>
+        <Form
+            type="Create"
+            post={post}
+            setPost={setPost}
+            submitting={submitting}
+            handleSubmit={createPrompt}
+        >
+
+        </Form>
     )
 }
 
--- components/Form.jsx
+++ components/Form.jsx
@@ -1,8 +1,43 @@
 import React from 'react'
+import Link from 'next/link'
 
-const Form = () => {
+const Form = ({ 
+    type,
+    post,
+    setPost,
+    submitting,
+    handleSubmit,
+}) => {
   return (
-    <div>Form</div>
+    <section className='w-full max-w-full flex-start flex-col'>
+        <h2 className='head_text text-left'>
+            <span className='blue_gradient'>
+                {type} Post
+            </span>
+        </h2>
+        <p className='desc text-left max-w-md'>
+            {type} and share amazing prompts with the world, and let your imagination run wild with AI-powered platforms
+        </p>
+        <form
+            onSubmit={handleSubmit}
+            className='glassmorphism mt-10 w-full max-w-2x1 flex flex-col gap-7' 
+        >
+            <label htmlFor="">
+                <span className='font-satoshi font-semibold text-base text-grey-700'>
+                    Your AI prompt
+                </span>
+            </label>
+            <textarea 
+                className='form_text-area'
+                value={post.prompt}
+                onChange={(e)=>{
+                    setPost({...post, prompt: e.target.value})
+                }}
+                placeholder='Write your prompts here...'
+            >
+            </textarea>
+        </form>
+    </section>
   )
 }
 
